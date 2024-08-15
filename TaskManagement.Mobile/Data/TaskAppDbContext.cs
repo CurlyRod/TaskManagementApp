8449d1f7cfe0e64937319419fccd94734db45a9f
Rod Mark Rufino <rodmark.rufino@blackfort.ph>
2024-08-15 13:14:39 +0800

    add db context and sqlite connector

---
+++ TaskManagement.Mobile/Data/TaskAppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Mobile.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace TaskManagement.Mobile.Data
+{
+
+    public class TaskAppDbContext 
+    {
+
+        public SQLiteAsyncConnection _dbConnection;
+        private readonly string nameSpace = typeof(TaskAppDbContext).Namespace;
+        public const string dbFileName = "taskapp.db";
+        public static string dbPathName => Path.Combine(FileSystem.AppDataDirectory, dbFileName);
+      
+        public const SQLite.SQLiteOpenFlags flags = 
+                             SQLiteOpenFlags.ReadOnly |  
+                             SQLiteOpenFlags.Create | 
+                             SQLiteOpenFlags.SharedCache;
+        public TaskAppDbContext()
+        {
+            if( _dbConnection is null)
+            {   
+               _dbConnection = new SQLiteAsyncConnection(dbPathName, flags);
+               _dbConnection.CreateTableAsync<UserInfoEntities>();
+            }    
+        }  
+      
+    }
+}
--- TaskManagement.Mobile/Pages/Home/Home.razor.cs
+++ TaskManagement.Mobile/Pages/Home/Home.razor.cs
@@ -1,7 +1,16 @@
 namespace TaskManagement.Mobile.Pages.Home
 {
     public partial class Home
-    {
+    {   
+        // for test purpose only if this app have a background process.
+        public bool IsLoading { get; set; } = true;
+        protected override async Task OnInitializedAsync()
+        {   
+            
+            await Task.Delay(2000); // Set only for 2s for loading. Ui 
+            IsLoading = false; 
 
+        }
+        
     }
 }
