1f6dd4e76b2bae9e64b519858223a0c23ae5f335
CurlyRod <rufinorodmark7@gmail.com>
2024-08-26 23:29:41 +0800

    add task entities and model for migration

---
+++ TaskManagement.Mobile/Models/TaskModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Mobile.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; } 
+        public string CreatedDate { get; set; } 
+        public string TaskStatus { get; set; } 
+        public bool ActiveInactice { get; set; } 
+    }
+}
